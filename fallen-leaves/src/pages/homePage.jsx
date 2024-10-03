ea50b43829281d472921e08d35a7f10c1174441a
Pantonym <221179@virtualwindow.co.za>
2024-10-03 21:14:36 +0200

    Deployed new version and added home page form functionality

--- fallen-leaves/src/pages/homePage.jsx
+++ fallen-leaves/src/pages/homePage.jsx
@@ -5,25 +5,76 @@ import { Oval } from 'react-loader-spinner';
 import { useNavigate } from 'react-router-dom';
 import { getUserProfile } from '../services/userService';
 import { useAuth } from '../contexts/authContext';
-import { getUserHabits } from '../services/habitService';
+import { addEntryToHabit, addNewHabit, checkHabitExists, getUserHabits } from '../services/habitService';
 
 function HomePage() {
     // Form Controls
     const [entryFormShow, setEntryFormShow] = useState(false);
     const [habitFormShow, setHabitFormShow] = useState(false);
+    const [selectedHabit, setSelectedHabit] = useState('');
+    const [selectedGoal, setSelectedGoal] = useState('');
     // Loading Controller
     const [loading, setLoading] = useState(true);
     // Navigation
     const navigate = useNavigate();
     // User Data
     const { currentUser } = useAuth();
+    const [userID, setUserID] = useState();
     const [username, setUsername] = useState("USERNAME");
     const [barData, setBarData] = useState(null);
+    const [unit, setUnit] = useState('');
+    const [habits, setHabits] = useState([]);
+    const [entries, setEntries] = useState([]);
+    const [entryValue, setEntryValue] = useState(0);
+    const [selectedHabitToAddEntry, setSelectedHabitToAddEntry] = useState('')
 
     // Navigate to the insights page
     const handleNavigateInsightsPage = () => {
         navigate('/insights');
     }
+
+    // HABIT FORM
+    // --Handle habit selection
+    const handleHabitChange = (e) => {
+        setSelectedHabit(e.target.value);
+    };
+
+    // --Handle goal selection
+    const handleGoalChange = (e) => {
+        setSelectedGoal(e.target.value);
+    };
+
+    // --Entry form habit select
+    const handleHabitSelect = (e) => {
+        const selectedHabit = habits.find(habit => habit.id === e.target.value);
+        setSelectedHabitToAddEntry(selectedHabit);
+        setUnit(habitUnits[selectedHabit.habitName]);
+    };
+
+    // HABIT LORE
+    // --Habit Descriptions
+    const habitDescriptions = {
+        recycling: 'Recycling helps reduce waste by converting materials into reusable objects.',
+        composting: 'Composting turns organic waste into valuable fertilizer for your garden.',
+        energyUsage: 'Energy conservation reduces your carbon footprint and saves on utility bills.',
+        waterConservation: 'Saving water helps preserve our planet`s most vital resource.',
+        reusableBags: 'Using reusable bags reduces plastic waste and pollution.',
+    };
+    // --Habit Units
+    const habitUnits = {
+        recycling: 'kg',
+        composting: 'kg',
+        energyUsage: 'kWh',
+        waterConservation: 'liters',
+        reusableBags: 'count'
+    };
+
+    // --Collect user info
+    useEffect(() => {
+        if (currentUser) {
+            setUserID(currentUser.uid);
+        }
+    }, [currentUser]);
 
     useEffect(() => {
         const fetchUserHabitsData = async () => {
@@ -32,6 +83,13 @@ function HomePage() {
                     // TODO: Scaling based on the data set with the smallest numbers
                     // TODO: Change labels to include the units each item is made of
                     const userHabits = await getUserHabits(currentUser.uid);
+                    setHabits(userHabits);
+
+                    // --Default habit selection
+                    if (userHabits.length > 0) {
+                        setSelectedHabitToAddEntry(userHabits[0]);
+                        setUnit(habitUnits[userHabits[0].habitName]);
+                    }
 
                     // Use only the first two habits
                     const firstTwoHabits = userHabits.slice(0, 2);
@@ -72,8 +130,6 @@ function HomePage() {
             }
         };
 
-        fetchUserHabitsData();
-
         const fetchUsername = async () => {
             if (currentUser) {
                 getUserProfile(currentUser.uid).then((data) => {
@@ -84,10 +140,75 @@ function HomePage() {
             }
         };
 
-
+        fetchUserHabitsData();
         fetchUsername();
         setLoading(false);
     }, []);
+
+    // --Habit confirm click
+    const handleHabitConfirmClick = async () => {
+        // Check if a habit is selected
+        if (selectedHabit === '') {
+            alert('Please select a habit.');
+            return;
+        }
+
+        try {
+            console.log(userID);
+
+            // TODO: Only show habits the user doesn't have
+            // Check if the habit already exists
+            const habitExists = await checkHabitExists(userID, selectedHabit);
+            if (habitExists) {
+                alert('You already have this habit.');
+                return;
+            }
+
+            // Add the habit if it doesn't already exist
+            await addNewHabit(userID, selectedHabit, selectedGoal);
+            alert('Habit added successfully!');
+            setHabitFormShow(false);
+        } catch (error) {
+            console.error('Error adding habit:', error);
+            alert('An error occurred while adding the habit. Please try again.');
+        }
+    };
+
+    // ENTRY FORM
+    // --Entry value change
+    const handleEntryValueChange = (e) => {
+        setEntryValue(e.target.value);
+    };
+
+    // --Add an entry
+    const handleAddEntrySubmissionClick = async () => {
+        console.log('User ID:', userID);
+        console.log('Habit ID:', selectedHabitToAddEntry.id);
+
+        if (entryValue <= 0) {
+            alert('Please enter a valid number.');
+            return;
+        }
+
+        try {
+            const newEntry = {
+                date: new Date(),
+                value: entryValue,
+                unit: unit
+            };
+
+            // Add the entry to the Firestore document for the selected habit
+            await addEntryToHabit(userID, selectedHabitToAddEntry.id, newEntry);
+
+            // Update the displayed entries for the selected habit
+            setEntries([...entries, newEntry]);
+            setEntryFormShow(false); // Close the form
+            alert('Entry added successfully!');
+        } catch (error) {
+            console.error('Error adding entry:', error);
+            alert('An error occurred while adding the entry. Please try again.');
+        }
+    };
 
     // Loader
     if (loading) {
@@ -100,35 +221,73 @@ function HomePage() {
 
     return (
         <div>
+            {/* FORM TO ADD A HABIT */}
             {habitFormShow && (
                 <div className={styles.habitsForm}>
                     <h1 className={styles.fontWhite}>Add Habit</h1>
 
-                    <select id="addHabitDropdown" className={`${styles.addHabitSelect} inter_font`}>
-                        <option value="option1">Habit 1</option>
-                        <option value="option2">Habit 2</option>
-                        <option value="option3">Habit 3</option>
+                    {/* TODO: Future implementation, populate only with habits the user doesn't have */}
+                    <select id="addHabitDropdown" className={`${styles.addHabitSelect} inter_font`} onChange={handleHabitChange}>
+                        <option value="">Select a Habit</option>
+                        <option value="recycling">Recycling</option>
+                        <option value="composting">Composting</option>
+                        <option value="energyUsage">Energy Usage</option>
+                        <option value="waterConservation">Water Conservation</option>
+                        <option value="reusableBags">Reusable Bags</option>
                     </select>
 
-                    <button className='btnSecondaryDesktop'>Confirm</button>
+                    {/* Description of the selected habit */}
+                    {selectedHabit ? (
+                        <p className={styles.habitDescription}>
+                            {habitDescriptions[selectedHabit]}
+                        </p>
+                    ) : (
+                        // Spacer that stops rendering when the text is visible
+                        <div style={{ height: '25px', width: '1px' }}></div>
+                    )}
+
+                    <select id="addHabitDropdown" className={`${styles.addHabitSelect} inter_font`} onChange={handleGoalChange}>
+                        <option value="">Select a Goal</option>
+                        <option value="maintain">Maintain current value</option>
+                        <option value="reduce">Reduce current value</option>
+                    </select>
+
+                    <button className='btnSecondaryDesktop' onClick={handleHabitConfirmClick}>Confirm</button>
                     <button className='btnPrimaryDesktop' style={{ color: 'white' }} onClick={() => setHabitFormShow(false)}>
                         Cancel
                     </button>
                 </div>
             )}
 
+            {/* FORM TO ADD AN ENTRY */}
             {entryFormShow && (
                 <div className={styles.habitsForm}>
-                    <h1 className={styles.fontWhite}>Add Entry</h1>
+                    <h1 className={styles.fontWhite}>Add Entry (in {unit})</h1>
 
-                    <select id="habitDropdown" className={`${styles.habitSelect} inter_font`}>
-                        <option value="option1">Habit 1</option>
-                        <option value="option2">Habit 2</option>
-                        <option value="option3">Habit 3</option>
+                    {/* Dropdown to select habit */}
+                    <select
+                        value={selectedHabitToAddEntry ? selectedHabitToAddEntry.id : ''}
+                        onChange={handleHabitSelect}
+                        className={`${styles.habitSelect} inter_font`}
+                    >
+                        {habits.map(habit => (
+                            <option key={habit.id} value={habit.id}>
+                                {habit.habitName.charAt(0).toUpperCase() + habit.habitName.slice(1)}
+                            </option>
+                        ))}
                     </select>
-                    <input type='number' placeholder={0} min={0} className={styles.sedEntry}></input>
 
-                    <button className='btnSecondaryDesktop'>Confirm</button>
+                    {/* Input field for the entry value */}
+                    <input
+                        type='number'
+                        value={entryValue}
+                        placeholder={0}
+                        min={0}
+                        onChange={handleEntryValueChange}
+                        className={styles.sedEntry}
+                    />
+
+                    <button className='btnSecondaryDesktop' onClick={handleAddEntrySubmissionClick}>Confirm</button>
                     <button className='btnPrimaryDesktop' style={{ color: 'white' }} onClick={() => setEntryFormShow(false)}>
                         Cancel
                     </button>
